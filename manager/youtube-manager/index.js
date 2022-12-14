b7302a59418762025766703697d27534cb27a7cd
troli <trolidichvu@gmail.com>
2022-12-14 21:46:58 +0700

    add test db

--- manager/youtube-manager/index.js
+++ manager/youtube-manager/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 //mongoose
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://master:worker@cluster0.9rrfkpc.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://master:worker@cluster0.9rrfkpc.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri)
 
 //router
